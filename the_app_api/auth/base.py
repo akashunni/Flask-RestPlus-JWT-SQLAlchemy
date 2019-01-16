6973d2e2e9d9539e81d7025599c1f7e3c003b74e
akashunni <akashunni123@gmail.com>
2019-01-17 00:02:41 +0530

    Fixed crash when accessing a protected view without cookie

--- the_app_api/auth/base.py
+++ the_app_api/auth/base.py
@@ -1,17 +1,19 @@
 from functools import wraps
 from flask import abort
 from flask_jwt_extended import JWTManager, get_jwt_identity, verify_jwt_in_request
+from flask_jwt_extended.exceptions import NoAuthorizationError
 
 def jwt_required_with_role(the_role):
     def check(fn):
         @wraps(fn)
         def wrapper(*args, **kwargs):
-            verify_jwt_in_request()
+            try:
+                verify_jwt_in_request()
+            except NoAuthorizationError:
+                abort(401, 'Missing Authorization Header')
             identity = get_jwt_identity()
             if identity['role'] not in the_role:
-                abort(403, 'Admins only')
-                # return {"msg": 'Admins only!'}
-                # return jsonify(msg='Admins only!'), 403
+                abort(403, 'Permission Denied for ' + identity['role'])
             else:
                 return fn(*args, **kwargs)
         return wrapper
